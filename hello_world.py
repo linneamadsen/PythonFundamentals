cf3d8620ce35f5a80b5b5ad34461b3f5c47e26a2
Linnea Madsen <linneamadsen@gmail.com>
2020-08-20 22:11:55 -0500

    update hello world

--- hello_world.py
+++ hello_world.py
@@ -13,3 +13,5 @@ fave_food1 = "sushi"
 fave_food2 = "pizza"
 print("I love to eat {} and {}.".format(fave_food1, fave_food2)) # with .format()
 print(f"I love to eat {fave_food1} and {fave_food2}." ) # with an f string
+
+#add to my git
